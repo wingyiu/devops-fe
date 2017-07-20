9098a5d46ba8a4153410653a2416877e30fa4415
wingyiu <wyiu.lee@gmail.com>
2017-07-20 16:27:52 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,10 @@
 import Env from './env';
 
 let config = {
-    env: Env
+    SITE_TITLE: 'DEVOPS系统',
+    env: Env,
+    apiBaseUrl: 'http://devops.we.com/api/v1',
+    LS_KEY_PROFILE: 'ls-profile',
+    LS_KEY_APPID: 'ls-appid'
 };
 export default config;
