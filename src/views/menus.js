5778bb6edfc23f78e9e68a843165cce269b30cd4
wingyiu <wyiu.lee@gmail.com>
2017-07-20 16:16:52 +0800

    Create menus.js

---
+++ src/views/menus.js
@@ -0,0 +1,13 @@
+import consoleMenus from './console/menus';
+import projectMenus from './project/menus';
+import openMenus from './open/menus';
+import adminsMenus from './admin/menus';
+
+const menus = [
+    consoleMenus,
+    projectMenus,
+    openMenus,
+    adminsMenus
+]
+
+export default menus;
