e785dfaef77b6c1023c7e733596f0c1ffd029748
wingyiu <wyiu.lee@gmail.com>
2017-07-20 16:15:27 +0800

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,24 +1,29 @@
-<style scoped>
+<style>
     @import 'styles/common.css';
 </style>
 <template>
     <div>
-        <router-view></router-view>
+        <keep-alive>
+            <router-view v-if="$route.meta.keepAlive"></router-view>
+        </keep-alive>
+        <router-view v-if="!$route.meta.keepAlive"></router-view>
     </div>
 </template>
 <script>
     export default {
-        data () {
-            return {}
-        },
-        mounted () {
+        data() {
+            return {
 
+            };
         },
-        beforeDestroy () {
+        mounted() {
+            console.log('app.vue mounted');
+        },
+        beforeDestroy() {
 
         },
         methods: {
 
         }
-    }
+    };
 </script>
